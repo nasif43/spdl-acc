dd70944b6712a258a46239735b9a1c9444809d4a
Muhtasim <muhtasimhossain43@gmail.com>
2024-09-07 15:03:57 +0600

    vite.config.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  define: {
-    'process.env': process.env
-  }
+  plugins: [preact()]
 });
